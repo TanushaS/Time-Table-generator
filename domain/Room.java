c4253f861a75db88ecf5e98e210dc29c45b4e4d9
TanushaS <53893658+TanushaS@users.noreply.github.com>
2020-08-06 21:47:21 +0530

    Create Room.java

---
+++ domain/Room.java
@@ -0,0 +1,16 @@
+package ga.cs.domain;
+
+public class Room {
+	private String number;
+	private int seatingCapacity;
+	public Room(String number ,int seatingCapacity) {
+		this.number=number;
+		this.seatingCapacity=seatingCapacity;
+	}
+	public String getNumber() {
+		return number;
+	}
+	public int getSeatingCapacity() {
+		return seatingCapacity;
+	}
+}
